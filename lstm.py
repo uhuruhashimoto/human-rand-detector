3a6b2c8f954aacd8d47a37b5f88a74f02fa06d03
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-02-09 22:16:32 -0500

    technically it's started now

--- lstm.py
+++ lstm.py
@@ -1,26 +1,37 @@
 # LSTM from https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
+from tensorflow import keras
+from tensorflow.keras import layers
 import argparse
 
+# These may change with better training data
 parser = argparse.ArgumentParser(description='LSTM')
 parser.add_argument('-n', '--num_chars', type=int, default=1, help='Number of chars in each string')
+parser.add_argument('-l', '--num_lines', type=int, default=1, help='Number of lines (total strings)')
 args = parser.parse_args()
+
 
 if __name__ == "__main__":
     print(f"Number of chars in each string: {args.num_chars}")
-    n_test_str = 50
-    # load the training dataset
-    # Train dataset is 10 strings with labels
-    # Test set is 50 strings without labels
+    print(f"Number of strings: {args.num_lines}")
+    train_num = args.num_lines
+    train_len = args.num_chars
 
-    # Normalize strings by zero padding
-    n_hidden = 4
-    rnn_cell = LSTM(n_hidden)
+    # TODO: load the training dataset and labels
+
+    # Used https://www.tensorflow.org/guide/keras/rnn as tutorial
+    model = keras.Sequential()
+    model.add(layers.Embedding(input_dim=1000, output_dim=64))
+
+    # The output of GRU will be a 3D tensor of shape (batch_size, timesteps, 256)
+    model.add(layers.GRU(256, return_sequences=True))
+
+    # The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
+    model.add(layers.SimpleRNN(128))
+
+    model.add(layers.Dense(10))
+
+    model.summary()
