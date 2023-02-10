2c496490cda5a0459298ee6d7cbbcb392d4a82cb
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-02-09 21:57:27 -0500

    passing in input size

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # human-rand-detector
 Recurrent NN for binary classification of human and random binary strings
+
+# Usage
+`python3 lstm.py -n [num_chars_in_train_str]`
--- lstm.py
+++ lstm.py
@@ -8,15 +8,19 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+import argparse
+
+parser = argparse.ArgumentParser(description='LSTM')
+parser.add_argument('-n', '--num_chars', type=int, default=1, help='Number of chars in each string')
+args = parser.parse_args()
 
 if __name__ == "__main__":
+    print(f"Number of chars in each string: {args.num_chars}")
     n_test_str = 50
-    # load the dataset
+    # load the training dataset
     # Train dataset is 10 strings with labels
     # Test set is 50 strings without labels
 
     # Normalize strings by zero padding
-
-
     n_hidden = 4
     rnn_cell = LSTM(n_hidden)
