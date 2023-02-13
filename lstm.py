708754022b70cc09ae90f478fce8a8dcc83ca210
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-02-12 23:00:48 -0500

    get data into np arrays

--- lstm.py
+++ lstm.py
@@ -8,16 +8,10 @@ from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dropout, TimeDistributed, Dense, Activation, Embedding
 import argparse
 
-# These may change with better training data
-# parser = argparse.ArgumentParser(description='LSTM')
-# parser.add_argument('-n', '--num_chars', type=int, default=1, help='Number of chars in each string')
-# parser.add_argument('-l', '--num_lines', type=int, default=1, help='Number of lines (total strings)')
-# args = parser.parse_args()
-
 MODEL_DIR = './model'
 # TODO: temporary
-BATCH_SIZE = 64
-SEQ_LENGTH = 50
+BATCH_SIZE = 5
+SEQ_LENGTH = 1000
 
 def save_weights(epoch, model):
     if not os.path.exists(MODEL_DIR):
@@ -38,15 +32,9 @@ def build_model(batch_size, seq_len, vocab_size):
     model.add(Activation('softmax'))
     return model
 
-def char_to_int(c) -> int:
-    if c == '1':
-        return 1
-    else:
-        return 0
-
 def read_batches(T, vocab_size):
     # TODO: configure to read n strings of [1, 0] from csv file
-    arr = np.loadtxt("training.csv",
+    arr = np.loadtxt("labeled_data/training.csv",
                  delimiter=",", dtype=str)
     length = T.shape[0]
     batch_chars = length // BATCH_SIZE
@@ -60,6 +48,30 @@ def read_batches(T, vocab_size):
                 Y[batch_idx, i, T[batch_chars * batch_idx + start + i + 1]] = 1
         yield X, Y
 
+def char_to_int(c) -> int:
+    if c == '1':
+        return 1
+    else:
+        return 0
+
+# This gives us a 30 x 1000 array of 1s and 0s (30 strings)
+# and a 30 x 1 array of labels (0 or 1)
+def read_data_from_csv():
+    int_arr = np.ndarray(shape=(30, 1000), dtype=int)
+    int_labels = []
+    arr = np.loadtxt("labeled_data/training.csv",
+                 delimiter=",", dtype=str)
+    labels = np.loadtxt("labeled_data/labels.csv",
+                    delimiter=",", dtype=str)
+    # Convert labels to integers
+    for i in range(len(labels)):
+        int_labels.append(char_to_int(labels[i]))
+    int_labels = np.asarray(int_labels)
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            int_arr[i][j] = char_to_int(arr[i][j])
+    return int_labels, int_arr
+
 def train(model, data, epochs=1, batch_size=64, seq_len=50, vocab_size=2):
 
     model = build_model(batch_size, seq_len, vocab_size)
@@ -68,7 +80,7 @@ def train(model, data, epochs=1, batch_size=64, seq_len=50, vocab_size=2):
                   optimizer='adam', metrics=['accuracy'])
     for i in range(epochs):
         print(f"Epoch {i}")
-        T = np.asarray([char_to_int[c] for c in text], dtype=np.int32)
+        T = np.asarray([char_to_int[c] for c in data], dtype=np.int32)
         losses, accs = [], []
         for i, (X, Y) in enumerate(read_batches(T, vocab_size)):
             loss, acc = model.train_on_batch(X, Y)
@@ -82,3 +94,4 @@ def train(model, data, epochs=1, batch_size=64, seq_len=50, vocab_size=2):
 if __name__ == '__main__':
     model = build_model(64, 50, 2)
     model.summary()
+    read_data_from_csv()
